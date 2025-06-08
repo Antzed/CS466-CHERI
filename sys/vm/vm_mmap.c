59d73098d1f5b9bc62096e845bd1dcb3467eb9d2
jtlaughton <jtlaughton@gmail.com>
2025-06-07 23:48:06 -0500

    missing semi

--- sys/vm/vm_mmap.c
+++ sys/vm/vm_mmap.c
@@ -211,9 +211,9 @@
 	 perms = ~CHERI_PROT2PERM_MASK | vm_map_prot2perms(cap_prot);
 
 	 if(mrp->mr_extra != NULL){
-	 	perms |= CHERI_PERM_STORE_CAP | CHERI_PERM_LOAD_CAP
+	 	perms |= CHERI_PERM_STORE_CAP | CHERI_PERM_LOAD_CAP;
 	 }
-	 
+
 	 newcap = cheri_andperm(newcap, perms);
  
 	 return (newcap);
